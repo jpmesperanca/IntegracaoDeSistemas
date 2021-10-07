e5fd6a320078817ff49698aea1e211c2140f7267
Gonçalo Filipe Fernandes São Marcos <gmarcos@student.dei.uc.pt>
2021-10-07 19:59:06 +0100

    graph?

--- Projeto/graphs.py
+++ Projeto/graphs.py
@@ -13,19 +13,50 @@ def main():
     
     nOwnersList = []
     JSONAvgWList = []
+    JSONStdWList = []
+    JSONAvgRList = []
+    JSONStdRList = []
+    
+    MPackAvgWList = []
+    MPackStdWList = []
+    MPackAvgRList = []
+    MPackStdRList = []
+    
     
     for sete in sets:
         sete = sete.split("\n")
         nOwners = sete[0].split(" ")[2]
-        nOwnersList.append(nOwners)
+        nOwnersList.append(float(nOwners))
         
         JSONAvgW = sete[1].split(" ")[4]
-        JSONAvgWList.append(JSONAvgW)
+        JSONAvgWList.append(float(JSONAvgW))
         
         JSONStdW = sete[2].split(" ")[4]
-        JSONStdWList.append(JSONStdW)
-
+        JSONStdWList.append(float(JSONStdW))
         
+        JSONAvgR = sete[3].split(" ")[4]
+        JSONAvgRList.append(float(JSONAvgR))
+        
+        JSONStdR = sete[4].split(" ")[4]
+        JSONStdRList.append(float(JSONStdR))
+        
+        MPackAvgW = sete[5].split(" ")[4]
+        MPackAvgWList.append(float(MPackAvgW))
+        
+        MPackStdW = sete[6].split(" ")[4]
+        MPackStdWList.append(float(MPackStdW))
+        
+        MPackAvgR = sete[7].split(" ")[4]
+        MPackAvgRList.append(float(MPackAvgR))
+        
+        MPackStdR = sete[8].split(" ")[4]
+        MPackStdRList.append(float(MPackStdR))
+        
+        
+
+    plt.plot(JSONAvgWList, nOwnersList)
+    plt.show()
+            
         
 
 if __name__ == "__main__":
