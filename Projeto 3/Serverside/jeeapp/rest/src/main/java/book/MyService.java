8c6b0cc4f2b753bd9457f115371f85e02df8db5b
Josphze <43673761+Josphze@users.noreply.github.com>
2021-11-30 17:42:51 +0000

    Added rest method

--- Projeto 3/Serverside/jeeapp/rest/src/main/java/book/MyService.java
+++ Projeto 3/Serverside/jeeapp/rest/src/main/java/book/MyService.java
@@ -1,13 +1,14 @@
 package book;
 
-import java.sql.Time;
-import java.util.Calendar;
+import java.util.Map;
 
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.json.JSONObject;
 
 @RequestScoped
 @Path("/myservice")
@@ -17,20 +18,46 @@ public class MyService {
     @GET
     @Path("/test")
     public String method1() {
-        System.out.println("M1 executing....");
-        return "M1 executing...";
+        System.out.println("M1 executing...."); // Nao sei para onde isto vai
+        return "M1 baixo...";
     }
 
     @GET
-    @Path("/add")
-    public String method2() {
-        System.out.println("M2 executing....");
-        String name = "name_" + new Time(Calendar.getInstance().getTimeInMillis());
-        // manageStudents.addStudent(name);
-        return name;
+    @Path("/addManager")
+    public Map<String, Object> addManager() {
+
+        JSONObject jo = new JSONObject();
+        jo.put("name", "jon doe");
+        jo.put("age", "22");
+        jo.put("city", "chicago");
+
+        return jo.toMap(); // Works
+    }
+
+    @GET
+    @Path("/addManager2")
+    public JSONObject addManager2() {
+
+        JSONObject jo = new JSONObject();
+        jo.put("name", "jon doe");
+        jo.put("age", "22");
+        jo.put("city", "chicago");
+
+        return jo; // returns {"empty": false}
     }
 
     /*
+     * @GET
+     * 
+     * @Path("/add")
+     * public String method2() {
+     * System.out.println("M2 executing....");
+     * String name = "name_" + new Time(Calendar.getInstance().getTimeInMillis());
+     * // manageStudents.addStudent(name);
+     * return name;
+     * }
+     * 
+     * 
      * @GET
      * 
      * @Path("/list")
