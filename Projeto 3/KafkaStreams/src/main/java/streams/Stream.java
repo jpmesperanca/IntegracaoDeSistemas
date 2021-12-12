a00dc09a20f526d7387e67326038401dba9a9140
Josphze <43673761+Josphze@users.noreply.github.com>
2021-12-12 19:21:52 +0000

    Doesn't work

--- Projeto 3/KafkaStreams/src/main/java/streams/Stream.java
+++ Projeto 3/KafkaStreams/src/main/java/streams/Stream.java
@@ -27,7 +27,6 @@ public class Stream {
 		String paymentsTopic = "Payments";
 		Integer TIME_WINDOW_VALUE = 2; // EM MINUTOS <------
 		Integer TIME_WINDOW_P_VALUE = 3; // EM MINUTOS <------
-		// String dbinfoTopic = "DB Info";
 
 		// 7. Calculate credits per user
 		java.util.Properties props = new Properties();
@@ -184,6 +183,7 @@ public class Stream {
 		StreamsBuilder builder13 = new StreamsBuilder();
 
 		KStream<String, String> linesBwTW = builder13.stream(creditsTopic);
+
 		KTable<Windowed<String>, Double> countlinesBwTW = linesBwTW.mapValues(v -> multiplyJson(v))
 				.groupByKey(Grouped.with(Serdes.String(), Serdes.Double()))
 				.windowedBy(TimeWindows.of(Duration.ofMinutes(TIME_WINDOW_VALUE)))
@@ -199,7 +199,7 @@ public class Stream {
 		// 14. Get the list of clients without payments for the last two months
 
 		java.util.Properties props14 = new Properties();
-		props14.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkastreamsPwTW"); // Bill with Time Window
+		props14.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkastreamsPwTW"); // Payments with Time Window
 		props14.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		props14.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 		props14.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
@@ -220,7 +220,32 @@ public class Stream {
 
 		// 15. Get the data of the person with the highest outstanding debt (i.e., the
 		// most current balance).
-
+		// Does not work
+		/*
+		 * java.util.Properties props15 = new Properties();
+		 * props15.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkastreamsHOD");
+		 * props15.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		 * props15.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
+		 * Serdes.String().getClass());
+		 * props15.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
+		 * Serdes.String().getClass());
+		 * StreamsBuilder builder15 = new StreamsBuilder();
+		 * 
+		 * KStream<String, String> clientsStream = builder15.stream("DBInfoClients");
+		 * 
+		 * clientsStream.mapValues((k, v) -> getDebt(v))
+		 * .groupByKey(Grouped.with(Serdes.String(), Serdes.Double()))
+		 * .aggregate(
+		 * () -> 0.0,
+		 * (key, value, total) -> Math.max(value, total))
+		 * .mapValues((k, v) -> insertPaymentsTimedInClientJson(v))
+		 * .toStream()
+		 * .to("Totais", Produced.with(Serdes.String(), Serdes.String()));
+		 * 
+		 * KafkaStreams streamsHOD = new KafkaStreams(builder15.build(), props15);
+		 * 
+		 * streamsHOD.start();
+		 */
 		// 16. Get the data of the manager who has made the highest revenue in payments
 		// from their clients.
 
